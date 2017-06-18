4a38bdb16daddfdbb23f09df05e2de99d92fd1a8
eilonweinfeld <eilonweinfeld.ew@gmail.com>
2017-06-18 11:22:22 +0300

    fix

--- HW3/src/BattleThreadPool.cpp
+++ HW3/src/BattleThreadPool.cpp
@@ -11,10 +11,8 @@
 
 bool BattleThreadPool::runBattle(AlgoDLL *dllA, AlgoDLL *dllB, GameData *bd)
 {
-	clock_t stAll = clock();
 	unique_ptr<IBattleshipGameAlgo> playerA, playerB;
 	playerA = unique_ptr<IBattleshipGameAlgo>({ dllA->GetAlgoInstance() });
-	//playerA= unique_ptr<IBattleshipGameAlgo>(new BattleShipGameSmartAlgo());
 	playerB= unique_ptr<IBattleshipGameAlgo>({ dllB->GetAlgoInstance() });
 	GameData gameData;
 	bd->clone(gameData);
@@ -24,29 +22,16 @@ bool BattleThreadPool::runBattle(AlgoDLL *dllA, AlgoDLL *dllB, GameData *bd)
 	playerA->setBoard(playerAData);
 	playerB->setPlayer(1);
 	playerB->setBoard(playerBData);
-
 	int currentTurn = 0;// A turn when 0 , B turn when 1;
-	
 	bool playerAFin = false; bool playerBFin = false;
 	int playerAScore = 0; int playerBScore = 0;
-	clock_t clksAattack = 0;
-	clock_t clksAnotify = 0;
-	clock_t clksBattack = 0;
-	clock_t clksBnotify = 0;
 
 	while (true) {
 		if (playerAFin && playerBFin) {
 			// ask ALON what to do here???? who wins?!
 			return true;
 		}
-		clock_t st = clock();
 		auto currAttack = (currentTurn == A_TURN) ? playerA->attack() : playerB->attack();
-		if (currentTurn == A_TURN) {
-			clksAattack += (clock() - st);
-		}
-		else {
-			clksBattack += (clock() - st);
-		}
 		if (currAttack == Coordinate(-1, -1,-1)) {
 			//mark player finished , switch turns and continue
 			(currentTurn == A_TURN) ? playerAFin = true : playerBFin = true;
@@ -86,38 +71,24 @@ bool BattleThreadPool::runBattle(AlgoDLL *dllA, AlgoDLL *dllB, GameData *bd)
 				break;
 			}
 		}
-		st = clock();
 		playerA->notifyOnAttackResult(currentTurn, currAttack, unifiedRes); //notify A
-		clksAnotify += (clock() - st);
-		st = clock();
 		playerB->notifyOnAttackResult(currentTurn, currAttack, unifiedRes); //notify B
-		clksBnotify += (clock() - st);
 
 		if (!gameData.fleetA.isNotLose()) { // a lost meaning b won
 			dllA->addLose();
 			dllB->addWin();
-			/*return true;*/
-			break;
+			return true;
 		}
 		if (!gameData.fleetB.isNotLose()) { // B lost meaning A won
 			dllA->addWin();
 			dllB->addLose();
-			/*return true;*/
-			break;
+			return true;
 		}
 		if (shouldSwitchTurn) {
 			currentTurn = (currentTurn + 1) % 2;
 		}
 		continue;
 	}
-	double allTime = double(clock() - stAll) / CLOCKS_PER_SEC;
-	double Anotify = double(clksAnotify) / CLOCKS_PER_SEC;
-	double Aattack = double(clksAattack) / CLOCKS_PER_SEC;
-	double Bnotify = double(clksBnotify) / CLOCKS_PER_SEC;
-	double Battack = double(clksBattack) / CLOCKS_PER_SEC;
-	cout << "game took " << allTime<<endl;
-	cout << "A, notify: " << Anotify << ", attack: " << Aattack << endl;
-	cout << "B, notify: " << Bnotify << ", attack: " << Battack << endl;
 	return true;
 }
 
